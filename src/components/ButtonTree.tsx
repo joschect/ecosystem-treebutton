3625a1f5e3e28853aa9704ac5e66df85c369cf50
Jon S <joschect@microsoft.com>
2021-01-08 10:51:37 -0800

    remove unneeded things

--- src/components/ButtonTree.tsx
+++ src/components/ButtonTree.tsx
@@ -5,26 +5,48 @@ export interface ButtonProps {
   /**
    * Text for your fancy button! Really should be able to use different colors though.
    */
-text: string;
-      /**
-       * Click to do things! Rather than not doing things, which is bad
-       */
-      onClick?: (ev: React.MouseEvent<any>) => void;
+  text: string;
+
+  /**
+   * Click to do things! Rather than not doing things, which is bad
+   */
+  onClick?: (ev: React.MouseEvent<any>) => void;
+
+  /**
+   * @default []
+   */
+  ornaments?: JSX.Element[];
 }
+
+export type Trunkation = 'trunk' | 'hide' | 'crown' | 'bark';
+export type ButtonTreeAlignment = 'evergreen' | 'deciduous';
 
 /**
  * All of the amazing props that you can pass to your
  * tall and evergrowing tree of buttons
  */
 export interface ButtonTreeProps {
-/**
- * An array of all of your buttons! They'll magically arranged in our favorite shape,
- * a tree! sure it's not always balanced right but who is counting?
- */
-buttons: ButtonProps[];
+
+  /**
+   * An array of all of your buttons! They'll magically arranged in our favorite shape,
+   * a tree! sure it's not always balanced right but who is counting?
+   */
+  buttons: ButtonProps[];
+
+  /**
+   * @default 'trunk'
+   */
+  trunkation?: Trunkation;
+
+  /**
+   * @default 'evergreen'
+   */
+  alignment?: ButtonTreeAlignment;
+
+
 }
 
-const RenderButtonTree = (props: { buttons: ButtonProps[]}) => {
+const RenderButtonTree = (props: ButtonTreeProps) => {
   let countPerRow = 1;
   let cc = 0;
   let row = 0;
@@ -52,13 +74,13 @@ const RenderButtonTree = (props: { buttons: ButtonProps[]}) => {
   return ret.map(v => {
     return <Stack horizontalAlign={'center'} horizontal>
       {v.map( b => (<Button onClick={b.onClick}> {b.text} </Button>))}
-    </Stack>
+      </Stack>
   });
 }
 
 export const ButtonTree = (props: ButtonTreeProps) => {
   return <div>
-  {RenderButtonTree({buttons: props.buttons})}
-  </div>
+    {RenderButtonTree({buttons: props.buttons})}
+    </div>
 }
 
